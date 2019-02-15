1e25286dcbc5f67aae82795dfd29c254360db2a8
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2019-02-15 12:24:41 +0900

    first_commit

---
+++ database/seeds/TodosTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todos')->truncate();
+        DB::table('todos')->insert([
+            [
+                'title'      => 'Laravel Lessonを終わらせる',
+                'created_at' => Carbon::create(2018, 1, 1),
+                'updated_at' => Carbon::create(2018, 1, 4),
+            ],
+            [
+                'title'      => 'レビュー対策をする',
+                'created_at' => Carbon::create(2018, 2, 1),
+                'updated_at' => Carbon::create(2018, 2, 5),
+            ],
+        ]);
+    }
+}
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel</title>
+  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+  <!-- Fonts -->
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container py-5">
+    <h1 class="page-header">ToDo一覧</h1>
+    <p class="text-right">
+        <a class="btn btn-success" href="/todo/create">ToDoを追加</a>
+    </p>
+    <table class="table">
+      <thead class="thead-light">
+        <tr>
+          <th>やること</th>
+          <th>期限</th>
+          <th>作成日時</th>
+          <th>更新日時</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="align-middle">静的なTodoです</td>
+          <td class="align-middle">2018/01/01</td>
+          <td class="align-middle">2017/01/01</td>
+          <td class="align-middle">2017/01/10</td>
+          <td><a class="btn btn-primary" href="">編集</a></td>
+          <td><button class="btn btn-danger" type="submit"><i class="fas fa-trash-alt"></i></button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!-- Scripts -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
+
