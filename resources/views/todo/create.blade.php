d42939720bd7baeb24461c6ded498d4b07320191
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2019-02-15 13:14:59 +0900

    collectiveで書き換え

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -2,12 +2,12 @@
 @section ('content')
 
 <h2 class="mb-3">ToDo作成</h2>
-<form>
+{!! Form::open(['route' => 'todo.store']) !!}
   <div class="form-group">
-    <input type="text" class="form-control" placeholder="ToDo内容">
+    {!! Form::input('text', 'title', null, ['required', 'class' => 'form-control', 'placeholder' => 'ToDo内容']) !!}
   </div>
-  <button type="submit" class="btn btn-success float-right">追加</button>
-</form>
+  {!! Form::submit('追加', ['class' => 'btn btn-success float-right']) !!}
+{!! Form::close() !!}
 
 @endsection
 
--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -2,12 +2,12 @@
 @section ('content')
 
 <h2 class="mb-3">ToDo編集</h2>
-<form >
+{!! Form::open(['route' => ['todo.update', $todo->id], 'method' => 'PUT']) !!}
   <div class="form-group">
-    <input type="text" class="form-control" placeholder="ToDo内容">
+    {!! Form::input('text', 'title', $todo->title, ['required', 'class' => 'form-control']) !!}
   </div>
-  <button type="submit" class="btn btn-success float-right">更新</button>
-</form>
+  {!! Form::submit('更新', ['class' => 'btn btn-success float-right']) !!}
+{!! Form::close() !!}
 
 @endsection
 
